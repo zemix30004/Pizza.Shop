b88854c2ac8f6c0a8f45be47422999679d3bb1a1
zemix30004 <eugenezm@gmail.com>
2021-08-19 15:39:00 +0300

    initial commit

--- resources/views/order_place.blade.php
+++ resources/views/order_place.blade.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=D, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Итоговый результат заказа</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('title-block')
+
+Ваш заказ принят!
+
+@endsection
+
+@section('content')
+
 <h1>Ваш заказ принят!</h1>
-</body>
-</html>
+
+@endsection
 
 
 
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href=<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <title>Document</title>
-</head>
+@extends('layouts.app')
 
-<body>
+@section('content')
+
     <table border="1">
         <tr>
             <td>id</td>
@@ -73,5 +66,5 @@
     <p>Очень вкусная популярная пицца</p>
     <a class="btn btn-success" href="">Добавить в корзину</a>
 </div>
-</body>
-</html>
+
+@endsection
